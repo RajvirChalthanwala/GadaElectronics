85305ed4472e2ecc905813797d4860044ebf0374
Rajvir Chalthanwala <105453375+RajvirChalthanwala@users.noreply.github.com>
2022-10-14 01:33:20 -0400

    Finall Update (Except About us)

--- Models/TonyTv.cs
+++ Models/TonyTv.cs
@@ -10,18 +10,34 @@ namespace GadaElectronics.Models
     public class TonyTv
     {
         public int Id { get; set; }
+
         [Display(Name = "Brand Name")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Brand { get; set; }
 
-        [Column(TypeName = "decimal(18, 2)")]
+       
+        [Range(1, 3000)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string OriginCountry { get; set; }
+
+
+        [Range(1, 3000)]
         public int SerialNumber { get; set; }
+
         public string DisplayClearity { get; set; }
+
         public int Size { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "Launch Date")]
         public DateTime LaunchDate { get; set; }
-
+       
+        [Range(1, 3000)]
         public int TvRatings { get; set; }
     }
 }
